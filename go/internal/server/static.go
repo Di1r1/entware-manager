78fc95946ad54964c25225f370a30378dd6a1162
Di1r1 <di1r1@gitverse.ru>
2026-08-20 12:14:35 +0300

    v1.10.1: логотип EM на экране входа вместо иконки замка (login-logo.png 256px, покрывает весь круг, кэш v=2), style.css v=42

--- go/internal/server/static.go
+++ go/internal/server/static.go
@@ -25,6 +25,7 @@ var staticWhitelist = map[string]bool{
 	"/icons.svg":                  true,
 	"/favicon.png":                true,
 	"/favicon.ico":                true,
+	"/login-logo.png":             true,
 	"/version.json":               true,
 	"/lib/utils.js":               true,
 	"/menu/menu.js":               true,
